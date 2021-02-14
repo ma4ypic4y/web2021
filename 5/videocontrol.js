9f96664359b78aa1876324c80ad65de3e2066605
Maksim Borisov <maksppap@mail.ru>
2021-02-14 16:20:31 +0300

    Create videocontrol.js
    1

---
+++ 5/videocontrol.js
@@ -0,0 +1,25 @@
+var myVideo = document.getElementById("video1");
+
+function playPause() {
+    if (myVideo.paused)
+        myVideo.play();
+    else
+        myVideo.pause();
+}
+
+function makeBig() {
+    myVideo.width = 1300;
+    myVideo.height = 700;
+}
+
+function makeSmall() {
+    myVideo.width = 500;
+    myVideo.height = 400;
+}
+
+function makeNormal() {
+    myVideo.width = 1050;
+    myVideo.height = 590;
+
+    
+}
