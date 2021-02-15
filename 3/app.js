2667bf7be76245a30515328b381d20f4c7ff2301
Maksim Borisov <maksppap@mail.ru>
2021-02-15 10:35:30 +0300

    123

--- 3/app.js
+++ 3/app.js
@@ -1,10 +1,25 @@
+// function Complete() {
+//     // alert("PI");
+//     var Elem = "Surname: " + document.Form.userSurname.value +
+//         "\nName: " + document.Form.userName.value +
+//         // "\nDay: " + document.Form.day.value +
+//         "\nLogin: " + document.Form.login.value
+
+//     alert(Elem);
+// }
+
+
 
 function Complete() {
-    // alert("PIZDA");
-    var Elem = "Surname: " + document.Form.userSurname.value +
-        "\nName: " + document.Form.userName.value +
-        // "\nDay: " + document.Form.day.value +
-        "\nLogin: " + document.Form.login.value
-
-    alert(Elem);
+    // alert("PA");
+    alert("123")
+    alert(document.Form.userSurname.value)
+    document.getElementById("info").innerHTM = userName
+    document.getElementById("info").innerHTML = userSurname
+    
+}
+function Add() {
+    alert("Done.")
+    userName = document.Form.userSurname.value
+    userSurname = document.Form.userName.value
 }
