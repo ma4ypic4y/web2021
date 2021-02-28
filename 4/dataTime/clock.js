c8bf893d351ca1f294e05a7e9c5f10fe48eba7e5
Maksim Borisov <maksppap@mail.ru>
2021-02-28 04:24:58 +0300

    Update clock.js

--- 4/dataTime/clock.js
+++ 4/dataTime/clock.js
@@ -25,9 +25,9 @@ function setTime() {
     const seconds = time.getSeconds()
    
 
-    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`
-    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
-    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
+    // hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`
+    // minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
+    // secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
 
     timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} `
     dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
